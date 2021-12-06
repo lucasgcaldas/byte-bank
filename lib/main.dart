b0b66121562b1b6f1653bf3831c7a2460bdc193a
Lucas Gomes <lggomes007@gmail.com>
2021-12-06 19:52:35 -0300

    Implementing the transfer item design

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('1000,00'),
+                subtitle: Text('1234'),
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('2000,00'),
+                subtitle: Text('1234'),
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('3000,00'),
+                subtitle: Text('1234'),
+              )
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
+          onPressed: () {},
           child: Icon(Icons.add),
         ),
       ),
